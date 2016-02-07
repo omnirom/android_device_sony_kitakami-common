d3ded8908d28a0ba5dc5e6c47c7125c59bc02b3a
Humberto Borba <humberos@gmail.com>
2016-02-07 20:45:37 -0200

    kitakami: Call platform.mk from platform_omni.mk
    Also, move specific GSM config to gsm devices.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I0a9ccc6cf2a4e97d29ff5567d8874575ae99a60c

--- platform_omni.mk
+++ platform_omni.mk
@@ -14,10 +14,9 @@
 
 SONY_ROOT = device/sony/kitakami-common/rootdir
 
+# Common config
+include device/sony/kitakami-common/platform.mk
+
 # Copying files
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/twrp.fstab:recovery/root/etc/twrp.fstab
-
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    telephony.lteOnGSMDevice=1
