201829b4ad9df3d0adde47b331a3f6982c7e04fe
Humberto Borba <humberos@gmail.com>
2016-02-08 01:08:26 -0200

    kitakami: Remove DSPManager for now
    audio_effects is broken then it was removed
    from upstream.
    
    this patch remove DSPManager from build packages list.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I9e5528538e76ae29c16a95346007226d30011a51

--- PlatformConfigOmni.mk
+++ PlatformConfigOmni.mk
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Common
 include device/sony/kitakami-common/PlatformConfig.mk
+
+# Don't use DSPManager
+TARGET_NO_DSPMANAGER := true
