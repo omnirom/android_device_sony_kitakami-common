336bbfffe71b8d5ea7ca003d9a56341e48321938
Humberto Borba <humberos@gmail.com>
2016-02-07 23:06:19 -0200

    kitakami: Move TWRP config to common repo
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ifa859a92dd51383bfb83c56474404232f3bc419e

--- PlatformConfigOmni.mk
+++ PlatformConfigOmni.mk
@@ -13,27 +13,3 @@
 # limitations under the License.
 
 include device/sony/kitakami-common/PlatformConfig.mk
-
-# TWRP Recovery
-TARGET_NO_SEPARATE_RECOVERY := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-RECOVERY_SDCARD_ON_DATA := true
-TW_HAS_NO_RECOVERY_PARTITION := true
-TW_FLASH_FROM_STORAGE := true
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_INCLUDE_JB_CRYPTO := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/soc.0/f9824900.sdhci/by-name/userdata"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
-TW_CRYPTO_FS_FLAGS := "0x00000406"
-TW_CRYPTO_KEY_LOC := "footer"
-TW_INCLUDE_FUSE_EXFAT := true
-TW_NO_USB_STORAGE := true
-TW_NO_SCREEN_BLANK := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_MAX_BRIGHTNESS := 255
