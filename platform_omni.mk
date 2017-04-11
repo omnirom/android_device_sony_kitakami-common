f08295e570f87a898036d37a355d7b0c3e526b1b
Humberto Borba <humberos@gmail.com>
2017-04-11 00:50:06 -0300

    sony: kitakami: Add missed ro.vendor.extension_library property
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I6bd93ef7ec4c96d3567572ce375a4bb0be26a673

--- platform_omni.mk
+++ platform_omni.mk
@@ -25,6 +25,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     sensors.msm8994
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so
+
 # Copying files
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/twrp.fstab:recovery/root/etc/twrp.fstab
