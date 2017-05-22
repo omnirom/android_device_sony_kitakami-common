omnirom/android_device_sony_loire-common

sensors: use multihal
Change-Id: I0ec952f94c52fb40b3f8f7adb10dc80c62f7d383
Signed-off-by: Erik Castricum <git@cas-online.nl>

platform_omni.mk
@@ -24,6 +24,10 @@ include device/sony/loire-common/platform.mk
 PRODUCT_PACKAGES += \
     SnapdragonCamera
 
+# Sensor multihal
+PRODUCT_PACKAGES += \
+    sensors.msm8952
+
 # Copying files
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/twrp.fstab:recovery/root/etc/twrp.fstab

omnirom/android_device_sony_kitakami-common

platform_omni.mk
@@ -21,6 +21,10 @@ include device/sony/kitakami-common/platform.mk
 PRODUCT_PACKAGES += \
     SnapdragonCamera
 
+# Sensor multihal
+PRODUCT_PACKAGES += \
+    sensors.msm8994
+
 # Copying files
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/twrp.fstab:recovery/root/etc/twrp.fstab